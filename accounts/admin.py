89b90a003a910e5f812eb2aee7d03431dace66cf
Baba905 <baba78122@gmail.com>
2022-10-25 13:34:40 +0200

    add the class,make migration of them in the database, The  class: Product, Customer, Tag and Order

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'date_created']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'date_created']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['status', 'date_created']
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Customer,CustomerAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Tag,TagAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255, null= True)
+    phone = models.CharField(max_length=255, null= True)
+    email = models.EmailField(max_length=255, null= True)
+    date_created = models.DateTimeField(auto_now_add= True, null= True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY=(
+        ('Indoor', 'Indoor'),
+        ('Out door','Out door'),
+    )
+    name = models.CharField(max_length=255, null= True)
+    price = models.FloatField(null= True)
+    category = models.CharField(max_length=255, null= True, choices=CATEGORY)
+    description = models.CharField(max_length=255, null= True)
+    date_created = models.DateTimeField(auto_now_add= True, null= True, blank=True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Order(models.Model):
+    STATUS=(
+        ('Pending', 'Pending'),
+        ('Out of delivery', 'Out of delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    customer = models.ForeignKey(Customer, on_delete= models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete= models.SET_NULL, null=True)
+    date_created = models.DateTimeField(auto_now_add= True, null= True)
+    status = models.CharField(max_length=255,null =True, choices=STATUS)
